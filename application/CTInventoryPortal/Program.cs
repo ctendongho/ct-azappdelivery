31b9d100cc87dbdac99c417ddbf97f9b34eb6108
Charles Tendongho <ctendongho5@gmail.com>
2026-07-13 03:44:13 +0000

    Add Azure Monitor and Application Insights integration

--- application/CTInventoryPortal/Program.cs
+++ application/CTInventoryPortal/Program.cs
@@ -5,6 +5,8 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddApplicationInsightsTelemetry();
+
 builder.Services.Configure<ForwardedHeadersOptions>(options =>
 {
     options.ForwardedHeaders =
